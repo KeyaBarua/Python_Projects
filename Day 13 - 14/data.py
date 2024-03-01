9928a7878880c4765ed44c5e442aaf317ffb0e72
Keya Barua <52108484+KeyaBarua@users.noreply.github.com>
2024-03-01 16:56:44 +0600

    Create data.py
    Data list

---
+++ Day 13 - 14/data.py
@@ -0,0 +1,600 @@
+data = [{
+  'name': "Instagram",
+  'work':	"social media application",
+  'country': "United States",
+  'followers': 663.2
+},
+{
+  'name': "Cristiano Ronaldo",
+  'work':	"footballer",
+  'country': "Portugal",
+  'followers': 612.6
+},
+{
+  'name': "Leo Messi",
+  'work':	"footballer",
+  'country': "Argentina",
+  'followers': 493.9
+},
+{
+  'name': "Selena Gomez",
+  'work':	"musician",
+  'country': "United States",
+  'followers': 430.1
+},
+{
+  'name': "Kylie Jenner",
+  'work':	"media personality and a socialite",
+  'country': "United States",
+  'followers': 399
+},
+{
+  'name': "Dwayne Johnson(The Rock)",
+  'work':	"actor and a professional wrestler",
+  'country': "United States",
+  'followers': 392.9
+},
+{
+  'name': "Ariana Grande",
+  'work':	"musician",
+  'country': "United States",
+  'followers': 380.9
+},
+{
+  'name': "Kim Kardashian",
+  'work':	"media personality and a socialite",
+  'country': "United States",
+  'followers': 364.2
+},
+{
+  'name': "Beyonce",
+  'work':	"musician",
+  'country': "United States",
+  'followers': 318.9
+},
+{
+  'name': "Khloe Kardashian",
+  'work':	"media personality",
+  'country': "United States",
+  'followers': 311.7
+},
+{
+  'name': "Nike",
+  'work':	"athletic apparel brand",
+  'country': "United States",
+  'followers': 306.7
+},
+{
+  'name': "Kendall Jenner",
+  'work':	"model and media personality",
+  'country': "United States",
+  'followers': 294.4
+},
+{
+  'name': "Justin Bieber",
+  'work':	"musician",
+  'country': "United States",
+  'followers': 294.3
+},
+{
+  'name': "National Geographic",
+  'work':	"television network",
+  'country': "United States",
+  'followers': 283.7
+},
+{
+  'name': "Taylor Swift",
+  'work':	"musician",
+  'country': "United States",
+  'followers': 277.2
+},
+{
+  'name': "Virat Kohli",
+  'work':	"cricketer",
+  'country': "India",
+  'followers': 264.4
+},
+{
+  'name': "Jennifer Lopez",
+  'work':	"musician",
+  'country': "United States",
+  'followers': 253
+},
+{
+  'name': "Nicki Minaj",
+  'work':	"rapper and singer",
+  'country': "United States",
+  'followers': 228.3
+},
+{
+  'name': "Kourtney Kardashian",
+  'work':	"musician",
+  'country': "media personality and socialite",
+  'followers': 224.8
+},
+{
+  'name': "Neymar",
+  'work':	"footballer",
+  'country': "Brazil",
+  'followers': 216.8
+},
+{
+  'name': "Miley Cyrus",
+  'work':	"musician",
+  'country': "United States",
+  'followers': 215.7
+},      
+{
+  'name': "Katy Perry",
+  'work':	"musician",
+  'country': "United States",
+  'followers': 206.7
+},
+{
+  'name': "Zendaya",
+  'work':	"actress",
+  'country': "United States",
+  'followers': 185.1
+},
+{
+  'name': "Kevin Hart",
+  'work':	"comedian and actor",
+  'country': "United States",
+  'followers': 179.5
+},
+{
+  'name': "Cardi B",
+  'work':	"rapper",
+  'country': "United States",
+  'followers': 169.1
+},
+{
+  'name': "LeBron James",
+  'work':	"basketball player",
+  'country': "United States",
+  'followers': 159
+},
+{
+  'name': "Demi Lovato",
+  'work':	"musician",
+  'country': "United States",
+  'followers': 157.6
+},
+{
+  'name': "Rihanna",
+  'work':	"musician",
+  'country': "Barbados",
+  'followers': 152.7
+},
+{
+  'name': "Real Madrid C.F.",
+  'work':	"football club",
+  'country': "Spain",
+  'followers': 149.3
+},
+{
+  'name': "Chris Brown",
+  'work':	"singer-songwriter",
+  'country': "United States",
+  'followers': 144.8
+},
+{
+  'name': "Drake",
+  'work':	"rapper and singer",
+  'country': "Canada",
+  'followers': 144.4
+},
+{
+  'name': "Ellen DeGeneres",
+  'work':	"TV host",
+  'country': "United States",
+  'followers': 139.8
+},
+{
+  'name': "FC Barcelona",
+  'work':	"football club",
+  'country': "Spain",
+  'followers': 124.9
+},
+{
+  'name': "UEFA Champions League",
+  'work':	"annual club association football competition",
+  'country': "Europe",
+  'followers': 111.7
+},
+{
+  'name': "Billie Eilish",
+  'work':	"singer-songwriter",
+  'country': "United States",
+  'followers': 110.2
+},
+{
+  'name': "Kylian Mbappe",
+  'work':	"footballer",
+  'country': "France",
+  'followers': 110.1
+},
+{
+  'name': "Gal Gadot",
+  'work':	"actress and model",
+  'country': "Israel",
+  'followers': 109.5
+},
+{
+  'name': "Vin Diesel",
+  'work':	"actor",
+  'country': "United States",
+  'followers': 101.9
+},
+{
+  'name': "Lisa",
+  'work':	"rapper and singer",
+  'country': "Thailand",
+  'followers': 99.3
+},
+{
+  'name': "NASA",
+  'work':	"space agency",
+  'country': "United States",
+  'followers': 97.1
+},
+{
+  'name': "Shakira",
+  'work':	"musician",
+  'country': "Colombo",
+  'followers': 90
+},
+{
+  'name': "Priyanka Chopra",
+  'work':	"actress",
+  'country': "India",
+  'followers': 89.9
+},
+{
+  'name': "Dua Lipa",
+  'work':	"musician",
+  'country': "United Kingdom",
+  'followers': 88.5
+},
+{
+  'name': "David Beckham",
+  'work':	"former footballer",
+  'country': "United Kingdom",
+  'followers': 86.1
+},
+{
+  'name': "Shraddha Kapoor",
+  'work':	"actress",
+  'country': "India",
+  'followers': 84.6
+},
+{
+  'name': "NBA",
+  'work':	"professional basketball league",
+  'country': "United States",
+  'followers': 84.1
+},
+{
+  'name': "Snoop Dogg",
+  'work':	"rapper",
+  'country': "United States",
+  'followers': 83
+},
+{
+  'name': "Narendra Modi",
+  'work':	"prime minister",
+  'country': "India",
+  'followers': 81.4
+},
+{
+  'name': "Alia Bhatt",
+  'work':	"actress",
+  'country': "India",
+  'followers': 80.9
+},
+{
+  'name': "Khaby Lame",
+  'work':	"influencer",
+  'country': "Senegal, Italy",
+  'followers': 80.2
+},
+{
+  'name': "Gigi Hadid",
+  'work':	"model and TV personality",
+  'country': "United States",
+  'followers': 79
+},
+{
+  'name': "Deepika Padukone",
+  'work':	"actress",
+  'country': "India",
+  'followers': 77.6
+},
+{
+  'name': "Victoria's Secret",
+  'work':	"fashion brand",
+  'country': "United States",
+  'followers': 76.3
+},
+{
+  'name': "Karim Benzema",
+  'work':	"footballer",
+  'country': "France",
+  'followers': 76.2
+},
+{
+  'name': "Emma Watson",
+  'work':	"actress",
+  'country': "United Kingdom",
+  'followers': 75.1
+},
+{
+  'name': "Ronaldinho",
+  'work':	"former footballer",
+  'country': "Brazil",
+  'followers': 74.8
+},
+{
+  'name': "Rose",
+  'work':	"KPOP singer",
+  'country': "Korea",
+  'followers': 74.6
+},
+{
+  'name': "BTS_official",
+  'work':	"KPOP boy band",
+  'country': "Korea",
+  'followers': 74
+},
+{
+  'name': "Raffi Ahmad",
+  'work':	"actor, presenter",
+  'country': "Indonesia",
+  'followers': 73.8
+},
+{
+  'name': "Shawn Mendes",
+  'work':	"musician",
+  'country': "Canada",
+  'followers': 73.6
+},  
+{
+  'name': "Justin Timberlake",
+  'work':	"musician",
+  'country': "United States",
+  'followers': 72
+},
+{
+  'name': "Premier League",
+  'work':	"football league",
+  'country': "United Kingdom",
+  'followers': 71.4
+},
+{
+  'name': "Abel Tesfaye(The Weeknd)",
+  'work':	"musician",
+  'country': "Canadian",
+  'followers': 68.1
+},
+{
+  'name': "Marvel Entertainment",
+  'work':	"entertainment company",
+  'country': "United States",
+  'followers': 67.7
+},
+{
+  'name': "Salman Khan",
+  'work':	"actor",
+  'country': "India",
+  'followers': 67.4
+},
+{
+  'name': "Camila Cabello",
+  'work':	"singer-songwriter",
+  'country': "United States",
+  'followers': 67.1
+},
+{
+  'name': "Karol G",
+  'work':	"musician",
+  'country': "Colombo",
+  'followers': 67
+},
+{
+  'name': "Akshay Kumar",
+  'work':	"actor",
+  'country': "India",
+  'followers': 66.7
+},
+{
+  'name': "Tom Holland",
+  'work':	"actor",
+  'country': "United Kingdom",
+  'followers': 66.5
+},
+{
+  'name': "Anushka Sharma",
+  'work':	"actress",
+  'country': "India",
+  'followers': 66.3
+},
+{
+  'name': "Marcelo",
+  'work':	"footballer",
+  'country': "Brazil",
+  'followers': 66.1
+},
+{
+  'name': "Paris Saint-Germain(PSG)",
+  'work':	"football club",
+  'country': "France",
+  'followers': 65.7
+},
+{
+  'name': "Anitta",
+  'work':	"singer-songwriter",
+  'country': "Brazil",
+  'followers': 64.8
+},
+{
+  'name': "Will Smith",
+  'work':	"actor and rapper",
+  'country': "United States",
+  'followers': 64.7
+},
+{
+  'name': "Zlatan Ibrahimović",
+  'work':	"former footballer",
+  'country': "Sweden",
+  'followers': 63.9
+},
+{
+  'name': "Maluma",
+  'work':	"singer-songwriter",
+  'country': "Colombo",
+  'followers': 63.8
+},
+{
+  'name': "Manchester United",
+  'work':	"football club",
+  'country': "England",
+  'followers': 63.1
+},
+{
+  'name': "Mohamed Salah",
+  'work':	"footballer",
+  'country': "Egypt",
+  'followers': 63
+},
+{
+  'name': "Kim Taehyung",
+  'work':	"KPOP singer",
+  'country': "Korea",
+  'followers': 62.5
+},
+{
+  'name': "Sergio Ramos",
+  'work':	"footballer",
+  'country': "Spain",
+  'followers': 62.2
+},
+{
+  'name': "Leonardo DiCaprio",
+  'work':	"actor",
+  'country': "United States",
+  'followers': 62
+},
+{
+  'name': "Zac Efron",
+  'work':	"actor",
+  'country': "United States",
+  'followers': 61.9
+},
+{
+  'name': "Paul Pogba",
+  'work':	"footballer",
+  'country': "France",
+  'followers': 61.5
+},
+{
+  'name': "Bella Hadid",
+  'work':	"fashion model",
+  'country': "United States",
+  'followers': 61.1
+},
+{
+  'name': "Disha Patani",
+  'work':	"actress and model",
+  'country': "India",
+  'followers': 60.2
+},
+{
+  'name': "JuventusFC",
+  'work':	"football club",
+  'country': "Italy",
+  'followers': 59.9
+},
+{
+  'name': "Whindersson Nunes",
+  'work':	"Youtuber and comedian",
+  'country': "Brazil",
+  'followers': 59.7
+},
+{
+  'name': "Chanel",
+  'work':	"fashion brand",
+  'country': "France",
+  'followers': 59.2
+},
+{
+  'name': "Chris Hemsworth",
+  'work':	"actor",
+  'country': "Australia",
+  'followers': 58
+},
+{
+  'name': "Paulo Dybala",
+  'work':	"footballer",
+  'country': "Argentina",
+  'followers': 57.9
+},
+{
+  'name': "9gag",
+  'work':	"online platform and social media website",
+  'country': "Hong Kong",
+  'followers': 57.8
+},  
+{
+  'name': "BlackPink",
+  'work':	"KPOP girl band",
+  'country': "Korea",
+  'followers': 57.6
+},   
+{
+  'name': "Robert Downey Jr.",
+  'work':	"actor",
+  'country': "United States",
+  'followers': 57.3
+},
+{
+  'name': "Michelle Obama",
+  'work':	"attorney and author ",
+  'country': "United States",
+  'followers': 57.2
+},     
+{
+  'name': "Tata Werneck",
+  'work':	"actress and TV presenter",
+  'country': "Brazil",
+  'followers': 57
+},
+{
+  'name': "Joko Widodo",
+  'work':	"president",
+  'country': "Indonesia",
+  'followers': 56.5
+},
+{
+  'name': "Lady Gaga",
+  'work':	"musician and actress",
+  'country': "United States",
+  'followers': 56.4
+},
+{
+  'name': "Kriti Sanon",
+  'work':	"actress",
+  'country': "India",
+  'followers': 55.8
+},
+{
+  'name': "Adele",
+  'work':	"musician",
+  'country': "United Kingdom",
+  'followers': 55.6
+},
+{
+  'name': "Stephen Curry",
+  'work':	"basketball player",
+  'country': "United States",
+  'followers': 55.4
+},]
